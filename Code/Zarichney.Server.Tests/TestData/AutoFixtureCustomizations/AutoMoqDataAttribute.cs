cf7cf48d8ea17143833cd056a668618e46d65cd9
Zarichney-Development <steven@zarichney.com>
2025-09-09 16:58:24 -0400

    Revert "test: improve coverage with AI strategic analysis (#94)"
    This reverts commit 8b583c538f5698399bc4f773281c86c583fc0acc.

--- Code/Zarichney.Server.Tests/TestData/AutoFixtureCustomizations/AutoMoqDataAttribute.cs
+++ Code/Zarichney.Server.Tests/TestData/AutoFixtureCustomizations/AutoMoqDataAttribute.cs
@@ -13,7 +13,6 @@ using Moq;
 using Zarichney.Services.Email;
 using Zarichney.Services.Payment;
 using Microsoft.Extensions.Logging;
-using Zarichney.Server.Tests.Framework.TestData.AutoFixtureCustomizations;
 
 namespace Zarichney.Server.Tests.TestData.AutoFixtureCustomizations;
 
@@ -72,7 +71,6 @@ public class AutoMoqDataAttribute : AutoDataAttribute
     fixture.Customize(new EmailServiceCustomization());
     fixture.Customize(new HttpHeadersCustomization());
     fixture.Customize(new StripeObjectCustomization());
-    fixture.Customize(new WebScraperCustomization());
 
     // Configure AutoFixture to avoid infinite recursion with EF Core entities
     fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList()
